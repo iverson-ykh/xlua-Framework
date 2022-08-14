223c11755ffe0ff80d3b22e59566cf678b391c8c
iverson-ykh <915131918@qq.com>
2022-08-14 19:30:42 +0800

    ResourceManager finished

--- Assets/Scripts/Editor/BuildTool.cs
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -24,6 +25,8 @@ public class BuildTool : Editor
 
 
         List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
+        List<string> bundleInfos = new List<string>();
+
         string[] files = Directory.GetFiles(PathUtil.BuildResourcesPath,"*",SearchOption.AllDirectories);
         for (int i=0;i<files.Length;i++) {
             if (files[i].EndsWith(".meta")) { continue; }
@@ -37,14 +40,31 @@ public class BuildTool : Editor
             string bundleName = fileName.Replace(PathUtil.BuildResourcesPath,"").ToLower();
             assetBundle.assetBundleName =bundleName+ ".ab";
             assetBundleBuilds.Add(assetBundle);
+
+            List<string> dependenceInfo = GetDependence(assetName);
+            string bundleInfo = assetName + "|" + bundleName+".ab";
+            if (dependenceInfo.Count>0) {
+                bundleInfo = bundleInfo + "|" + string.Join("|",dependenceInfo);
+            }
+            bundleInfos.Add(bundleInfo);
         }
+        
+
         if (Directory.Exists(PathUtil.BuildOutPath)) {
             Directory.Delete(PathUtil.BuildOutPath,true);
         }
         Directory.CreateDirectory(PathUtil.BuildOutPath);
 
         BuildPipeline.BuildAssetBundles(PathUtil.BuildOutPath,assetBundleBuilds.ToArray(),BuildAssetBundleOptions.None,target);
-    }
+        File.WriteAllLines(PathUtil.BuildOutPath + "/" + AppConst.FileListName, bundleInfos);
 
+        AssetDatabase.Refresh();
+    }
+    static List<string> GetDependence(string curFile) {
+        List<string> dependence = new List<string>();
+        string[] files = AssetDatabase.GetDependencies(curFile);
+        dependence = files.Where(file=>!file.EndsWith(".cs")&&!file.Equals(curFile)).ToList();
+        return dependence;
+    }
     
 }
---
+++ Assets/Scripts/Framework/ResourceManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UObject = UnityEngine.Object;
+using System;
+
+public class ResourceManager : MonoBehaviour
+{
+    internal class BundleInfo {
+        public string AssetName;
+        public string BundleName;
+        public List<string> Dependences;
+    }
+    private Dictionary<string, BundleInfo> m_BundleInfos = new Dictionary<string, BundleInfo>();
+    private void ParseVersionFile() {
+        string url = Path.Combine(PathUtil.BuildOutPath,AppConst.FileListName);
+        string[] data = File.ReadAllLines(url);
+        for (int i=0;i<data.Length;i++) {
+            BundleInfo bundleInfo = new BundleInfo();
+            string[] info = data[i].Split('|');
+            bundleInfo.AssetName = info[0];
+            bundleInfo.BundleName = info[1];
+            bundleInfo.Dependences = new List<string>(info.Length-2);
+            for (int j=2;j<info.Length;j++) {
+                bundleInfo.Dependences.Add(info[j]);
+            }
+            m_BundleInfos.Add(bundleInfo.AssetName,bundleInfo);
+        }
+        
+        
+    }
+    IEnumerator LoadBundleAsync(string assetName, Action<UObject> action = null)
+    {
+        string bundleName = m_BundleInfos[assetName].BundleName;
+        string bundlePath = Path.Combine(PathUtil.BuildOutPath, bundleName);
+        List<string> dependences = m_BundleInfos[assetName].Dependences;
+        if (dependences != null && dependences.Count > 0)
+        {
+            for (int i = 0; i < dependences.Count; i++)
+            {
+                yield return LoadBundleAsync(dependences[i]);
+
+            }
+        }
+        AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(bundlePath);
+        yield return request;
+
+        AssetBundleRequest bundleRequest = request.assetBundle.LoadAssetAsync(assetName);
+        yield return bundleRequest;
+
+        action?.Invoke(bundleRequest?.asset);
+
+    }
+    public void LoadAsset(string assetName, Action<UObject> action)
+    {
+        StartCoroutine(LoadBundleAsync(assetName, action));
+    }
+
+    void Start()
+    {
+        ParseVersionFile();
+        LoadAsset("Assets/BuildResources/UI/Prefabs/TestUI.prefab",OnComplete);
+    }
+    private void OnComplete(UObject obj) {
+        GameObject go = Instantiate(obj) as GameObject;
+        go.transform.SetParent(this.transform);
+        go.SetActive(true);
+        go.transform.localPosition = Vector3.zero;
+    }
+}
